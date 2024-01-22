1e5d41ff4708b8faa4b881003460f98fe319600a
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-01-22 19:44:36 +0000

    Jest exercise

---
+++ app.js
@@ -0,0 +1,36 @@
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 156.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.87;
+    return valueInPound;
+    
+}
+
+
+
+
+
+
+// Esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum };
+
+
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
